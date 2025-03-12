53bd1c62a5043c59fc2a20b235fa8326d42001b7
TIROK547 <ilia.gotbi.2006@gmail.com>
2025-03-12 16:09:44 +0330

    finished

--- interaction/urls.py
+++ interaction/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from .views import add_item, get_items
 urlpatterns = [
-     
+    path("item/add/<str:name>",add_item),
+    path("item/get",get_items)
 ]
--- interaction/views.py
+++ interaction/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
+ITEMS = []
 
+def add_item(request,name):
+    global ITEMS
+    ITEMS.append(name)
+    return HttpResponse("item added")
+
+def get_items(request):
+    return HttpResponse(ITEMS)
+
+def home(request):
+    return render(request,"./templates/interaction/main.html")
